a4955fd54a23b2f3d9d2fa71eda9510afc20612a
Duv1s <dualgone070498@gmail.com>
2019-04-10 23:03:50 -0500

    Se crea el servicio de Crear Persona, Consultar Marcas  y Consultar Lineas.

---
+++ semilleroHBT/ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/main/java/com/hbt/semillero/dto/MarcaDTO.java
@@ -0,0 +1,32 @@
+package com.hbt.semillero.dto;
+
+import java.io.Serializable;
+
+public class MarcaDTO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Long idMarca;
+	private String nombre;
+	public Long getIdMarca() {
+		return idMarca;
+	}
+	public void setIdMarca(Long idMarca) {
+		this.idMarca = idMarca;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
+
+}
